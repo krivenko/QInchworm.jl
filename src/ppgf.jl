1d8d714c47851b05001d1cb817fcf413a30f28cc
Hugo U. R. Strand <hugo.strand@gmail.com>
2021-02-11 14:01:29 +0100

    [ppgf] vertex configuration evaluator

--- src/ppgf.jl
+++ src/ppgf.jl
@@ -4,11 +4,15 @@ module ppgf
 import LinearAlgebra: Diagonal
 
 import Keldysh: TimeGrid, TimeGF
+
 import KeldyshED: EDCore, energies, partition_function
+import KeldyshED: c_connection, cdag_connection
+import KeldyshED: c_matrix, cdag_matrix
 
-export AtomicPseudoParticleGF
+export atomic_ppgf
+export operator_product
 
-function AtomicPseudoParticleGF(grid::TimeGrid, ed::EDCore, β::Real)
+function atomic_ppgf(grid::TimeGrid, ed::EDCore, β::Real)
 
     G = Vector{TimeGF}()
     
@@ -25,5 +29,49 @@ function AtomicPseudoParticleGF(grid::TimeGrid, ed::EDCore, β::Real)
     end    
     return G
 end
+
+"""
+    operator_product(...)
+
+Evaluate a product of vertices at different contour times `z_i` with 
+the pseud-particle Green's function sandwitched in between.
+
+`vertices` is a contour-time ordered list of triples `(z_i, c_i, o_i)` were:
+  `z_i` is a contour time,
+  `c_i` is +1/-1 for creation/annihilation operator respectively, and 
+  `o_i` is a spin-orbital index
+"""
+function operator_product(ed::EDCore, G, s::Integer, z_i, z_f, vertices)
+    s_a = s
+    (z_a, c_a, o_a) = vertices[1]
+
+    prod0 = G[s_a][z_a, z_i]
+    prod = prod0
+
+    for (vidx, (z_a, c_a, o_a)) in enumerate(vertices)
+
+        connection = c_a > 0 ? cdag_connection : c_connection
+        matrix = c_a > 0 ? cdag_matrix : c_matrix
+
+        s_b = connection(ed, o_a, s_a)
+        s_b == nothing && return 0 * prod0
+
+        m_ba = matrix(ed, o_a, s_a)
+
+        if vidx < length(vertices)
+            z_b = vertices[vidx + 1][1]
+        else
+            z_b = z_f
+        end
+
+        prod = G[s_b][z_b, z_a] * m_ba * prod
+
+        s_a = s_b
+    end
+
+    s_a != s && return 0 * prod0
+
+    prod
+end;
 
 end # module ppgf
--- test/ppgf.jl
+++ test/ppgf.jl
@@ -2,15 +2,16 @@
 import Test.@test
 import Test.@testset
 
-import LinearAlgebra: Diagonal
+import LinearAlgebra: Diagonal, tr
 
 import Keldysh; kd = Keldysh;
 
-import KeldyshED;
+import KeldyshED; ked = KeldyshED;
 import KeldyshED.Hilbert;
 import KeldyshED; op = KeldyshED.Operators;
 
-import QInchworm.ppgf: AtomicPseudoParticleGF
+import QInchworm.ppgf: atomic_ppgf
+import QInchworm.ppgf: operator_product
 
 @testset "atomic ppgf" begin
 
@@ -19,12 +20,14 @@ import QInchworm.ppgf: AtomicPseudoParticleGF
     U = +2.0 # Local interaction
     V = -0.1 # Hybridization
     B = +0.0 # Magnetic field
+    μ = -0.1 # Chemical potential
 
     # Hubbard-atom Hamiltonian
 
     H = U * (op.n("up") - 1/2) * (op.n("do") - 1/2) 
     H += V * (op.c_dag("up") * op.c("do") + op.c_dag("do") * op.c("up")) 
     H += B * (op.n("up") - op.n("do"))
+    H += μ * (op.n("up") + op.n("do"))
 
     # Exact Diagonalization solver
     
@@ -67,9 +70,68 @@ import QInchworm.ppgf: AtomicPseudoParticleGF
         @test ρ_s ≈ G0_s[t_beta, t_0]
     end
 
-    G0_ref = AtomicPseudoParticleGF(grid, ed, β)
+    G0_ref = atomic_ppgf(grid, ed, β)
+    
     for (G0_s, G0_s_ref) in zip(G0, G0_ref)
         @test G0_s ≈ G0_s_ref
     end
 
+    # -- Compute Tr[\rho c^+_1 c_2]
+
+    n_ref = kd.density(g)[1]
+    
+    idx1 = d
+    idx2 = d
+
+    n::Complex = 0.
+
+    for (sidx1, s) in enumerate(ed.subspaces)
+
+        sidx2 = ked.c_connection(ed, idx1, sidx1)
+        sidx2 == nothing && continue
+
+        sidx3 = ked.cdag_connection(ed, idx2, sidx2)
+        sidx3 != sidx1 && continue
+
+        m_1 = ked.c_matrix(ed, idx1, sidx1)
+        m_2 = ked.cdag_matrix(ed, idx2, sidx2)
+
+        n += tr(ρ[sidx1] * m_2 * m_1)
+    end
+
+    @test n ≈ n_ref
+
+    # -- Compute single-particle Green's function using G0
+
+    g_tau = Vector{Real}()
+
+    for tau_i in getindex(grid, kd.imaginary_branch)
+        g_tau_i = 0.0im
+        
+        for (sidx1, s) in enumerate(ed.subspaces)
+
+            sidx2 = ked.cdag_connection(ed, idx1, sidx1)
+            sidx2 == nothing && continue
+
+            sidx3 = ked.c_connection(ed, idx2, sidx2)
+            sidx3 != sidx1 && continue
+
+            m_1 = ked.cdag_matrix(ed, idx1, sidx1)
+            m_2 = ked.c_matrix(ed, idx2, sidx2)
+
+            prod = G0[sidx1][t_beta, tau_i] * m_2 * G0[sidx2][tau_i, t_0] * m_1
+
+            # -- This is the general configuration evaluator function
+            prod_ref = operator_product(
+                ed, G0, sidx1, t_0, t_beta, [(t_0, +1, idx1),(tau_i, -1, idx2)])
+
+            @test isapprox(prod, prod_ref, atol=1e-12, rtol=1e-12)
+        
+            g_tau_i -= tr(prod)
+        end    
+        push!(g_tau, g_tau_i)
+    end
+    
+    @test isapprox(g_tau, g[:matsubara], atol=1e-12, rtol=1-12)
+    
 end
