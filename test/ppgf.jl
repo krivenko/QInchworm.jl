dfd3ab9357561167eae954940ddd4af5cff62146
Hugo U. R. Strand <hugo.strand@gmail.com>
2021-04-02 17:12:31 +0200

    [test] cleanup ppgf test

--- test/ppgf.jl
+++ test/ppgf.jl
@@ -2,7 +2,7 @@
 import Test.@test
 import Test.@testset
 
-import LinearAlgebra: Diagonal, tr
+import LinearAlgebra: Diagonal, ones, tr
 
 import Keldysh; kd = Keldysh;
 
@@ -36,7 +36,6 @@ import QInchworm.ppgf: first_order_spgf
     
     soi = KeldyshED.Hilbert.SetOfIndices([["up"], ["do"]]);
     ed = KeldyshED.EDCore(H, soi)
-    
     ρ = KeldyshED.density_matrix(ed, β)
     
     # Real-time Kadanoff-Baym contour
@@ -48,33 +47,10 @@ import QInchworm.ppgf: first_order_spgf
     
     u = KeldyshED.Hilbert.IndicesType(["up"])
     d = KeldyshED.Hilbert.IndicesType(["do"])
-    
-    g = KeldyshED.computegf(ed, grid, [(d, d)], β)[1];
-    
+        
     # Atomic propagator G0, array of (matrix valued) TimeGF one for each ed subspace
-    
-    G0 = [ kd.TimeGF(grid, length(s)) for s in ed.subspaces ];
-    
-    # Compute atomic propagator for all times
-    
-    Z = KeldyshED.partition_function(ed, β)
-    λ = log(Z) / β # Pseudo-particle chemical potential (enforcing Tr[G0(β)]=Tr[ρ]=1)
 
-    t_β = grid[kd.imaginary_branch][end]
-    N = operator_matrix_representation(total_density_operator(ed), ed)
-    
-    for (G0_s, E, n) in zip(G0, KeldyshED.energies(ed), N)
-        ξ = (-1)^n[1,1] # Statistics sign        
-        for t1 in grid, t2 in grid[1:t1.idx]
-            Δt = t1.val.val - t2.val.val
-            if t1.val.domain == kd.forward_branch && 
-               t2.val.domain != kd.forward_branch                
-                Δt += -im*β
-            end            
-            sign = ξ^(t1.idx > t_β.idx && t_β.idx >= t2.idx)
-            G0_s[t1, t2] = -im * sign * Diagonal(exp.(-1im * Δt * (E .+ λ)))
-        end
-    end
+    G0 = atomic_ppgf(grid, ed, β)
     
     t_0, t_beta = kd.branch_bounds(grid, kd.imaginary_branch)
     
@@ -82,20 +58,32 @@ import QInchworm.ppgf: first_order_spgf
         @test ρ_s ≈ im * G0_s[t_beta, t_0]
     end
 
-    G0_ref = atomic_ppgf(grid, ed, β)
+    # Check that propagation from 
+    # - t on fwd branch over t_max 
+    # - to the same time t on bwd branch
+    # is unity
     
-    for (G0_s, G0_s_ref) in zip(G0, G0_ref)
-        @test G0_s ≈ G0_s_ref
-    end
+    zb_max = grid[kd.backward_branch][1]
+    zf_max = grid[kd.forward_branch][end]
 
-    # -- Compute Tr[\rho c^+_1 c_2]
+    for (sidx, G_s) in enumerate(G0)
+        for (zb, zf) in zip(reverse(grid[kd.backward_branch]), grid[kd.forward_branch])
+            prod = im^2 * G_s[zb, zb_max] * G_s[zf_max, zf]
+            I = Diagonal(ones(size(prod, 1)))
+            @test prod ≈ I
+        end
+    end    
 
-    n_ref = kd.density(g)[1]
+    # -- Compute Tr[\rho c^+_1 c_2] using ED ρ and ppgf G0 cf spgf
+
+    g_ref = KeldyshED.computegf(ed, grid, [(d, d)], β)[1];
+    n_ref = kd.density(g_ref)[1]
     
     idx1 = d
     idx2 = d
 
-    n::Complex = 0.
+    n_rho::Complex = 0.
+    n_G0::Complex = 0.
 
     for (sidx1, s) in enumerate(ed.subspaces)
 
@@ -108,46 +96,19 @@ import QInchworm.ppgf: first_order_spgf
         m_1 = ked.c_matrix(ed, idx1, sidx1)
         m_2 = ked.cdag_matrix(ed, idx2, sidx2)
 
-        n += tr(ρ[sidx1] * m_2 * m_1)
+        n_rho += tr(ρ[sidx1] * m_2 * m_1)
+        n_G0 += tr(im * G0[sidx1][t_beta, t_0] * m_2 * m_1 )
     end
 
-    @test n ≈ n_ref
+    @test n_rho ≈ n_ref
+    @test n_G0 ≈ n_ref
 
-    # -- Compute single-particle Green's function using G0
-
-    g_tau = Vector{Real}()
-
-    for tau_i in getindex(grid, kd.imaginary_branch)
-        g_tau_i = 0.0im
-        
-        for (sidx1, s) in enumerate(ed.subspaces)
-
-            sidx2 = ked.cdag_connection(ed, idx1, sidx1)
-            sidx2 == nothing && continue
-
-            sidx3 = ked.c_connection(ed, idx2, sidx2)
-            sidx3 != sidx1 && continue
-
-            m_1 = ked.cdag_matrix(ed, idx1, sidx1)
-            m_2 = ked.c_matrix(ed, idx2, sidx2)
-
-            prod = (im)^2 * G0[sidx1][t_beta, tau_i] * m_2 * G0[sidx2][tau_i, t_0] * m_1
-            
-            # -- This is the general configuration evaluator function
-            prod_ref = operator_product(
-                ed, G0, sidx1, t_0, t_beta, [(t_0, +1, idx1),(tau_i, -1, idx2)])
-            
-            @test isapprox(prod, prod_ref, atol=1e-12, rtol=1e-12)
-
-            g_tau_i -= tr(prod)
-        end    
-        push!(g_tau, g_tau_i)
-    end
+    # Check spgf from ED and 1st order Inch
     
-    @test isapprox(g_tau, g[:matsubara], atol=1e-12, rtol=1-12)
-
-    g_ref = first_order_spgf(G0, ed, d, d)
-
-    @test isapprox(g, g_ref, atol=1e-12, rtol=1-12)
+    for (o1, o2) in [(u, u), (u, d), (d, u), (d, d)]
+        g = first_order_spgf(G0, ed, o1, o2);
+        g_ref = ked.computegf(ed, grid, [(o1, o2)], β)[1];
+        @test isapprox(g, g_ref, atol=1e-12, rtol=1-12)
+    end
     
 end
