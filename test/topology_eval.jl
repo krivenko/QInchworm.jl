02fe8ad9b7f9a5f994d9171e004049753f20eb63
Hugo U. R. Strand <hugo.strand@gmail.com>
2022-07-08 15:03:15 +0200

    [diag] small fixes

--- test/topology_eval.jl
+++ test/topology_eval.jl
@@ -79,19 +79,19 @@ import QInchworm.configuration: Configuration, Node, InchNode, NodePair, NodePai
     n_samples = 100
     
     for sample in range(1, n_samples)
+        
         # -- Generate time ordered points on the unit-interval (replace with quasi-MC points)
+        # -- separating the initial point `x1` (between the final- and worm-time) from the others `xs`
 
         x1 = Random.rand(Float64)
         xs = Random.rand(Float64, (2 * order - 1))
         sort!(xs, rev=true)
-
-        # -- separating the initial point `x1` (between the final- and worm-time) from the others `xs`
     
         # -- Map unit-interval points to contour times on the imaginary time branch
         τs = teval.timeordered_unit_interval_points_to_imaginary_branch_inch_worm_times(
             contour, worm_nodes, x1, xs)
 
-        # -- Check τs
+        # -- Sanity checks for the time points `τs`
         τ_f, τ_w, τ_0 = [ node.time for node in worm_nodes ]
 
         # First time τs[1] is between the final- and worm-time
