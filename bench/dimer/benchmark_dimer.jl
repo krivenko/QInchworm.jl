40d797917095bffa7fe91e388da2977f4bbce5e2
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-10-06 16:33:16 +0200

    [bench] Cleaned up `dimer`

--- bench/dimer/benchmark_dimer.jl
+++ bench/dimer/benchmark_dimer.jl
@@ -1,4 +1,4 @@
-using MPI
+using MPI; MPI.Init()
 
 using MD5
 using HDF5; h5 = HDF5
@@ -9,8 +9,7 @@ using Keldysh; kd = Keldysh
 using KeldyshED; ked = KeldyshED; op = KeldyshED.Operators;
 
 using QInchworm.ppgf
-using QInchworm.diagrammatics: get_topologies_at_order
-using QInchworm.expansion: Expansion, InteractionPair, get_diagrams_at_order
+using QInchworm.expansion: Expansion, InteractionPair
 
 using QInchworm.inchworm: inchworm!
 using QInchworm.spline_gf: SplineInterpolatedGF
@@ -19,47 +18,33 @@ using QInchworm.mpi: ismaster
 
 function run_dimer(nτ, orders, orders_bare, N_samples; interpolate_gfs=false)
 
-    if ismaster(); @show interpolate_gfs; end
-
     β = 1.0
     ϵ_1, ϵ_2 = 0.0, 2.0
     V = 0.5
 
-    # -- ED solution
+    # ED solution
 
-    H_dimer = ϵ_1 * op.n(1) + ϵ_2 * op.n(2) + V * ( op.c_dag(1) * op.c(2) + op.c_dag(2) * op.c(1) )
+    H_dimer = ϵ_1 * op.n(1) + ϵ_2 * op.n(2) +
+              V * (op.c_dag(1) * op.c(2) + op.c_dag(2) * op.c(1))
     soi_dimer = ked.Hilbert.SetOfIndices([[1], [2]])
     ed_dimer = ked.EDCore(H_dimer, soi_dimer)
 
-    # -- Impurity problem
+    # Impurity problem
 
-    contour = kd.ImaginaryContour(β=β);
-    grid = kd.ImaginaryTimeGrid(contour, nτ);
+    contour = kd.ImaginaryContour(β=β)
+    grid = kd.ImaginaryTimeGrid(contour, nτ)
 
     H = ϵ_1 * op.n(1)
     soi = ked.Hilbert.SetOfIndices([[1]])
     ed = ked.EDCore(H, soi)
 
-    ρ_ref = Array{ComplexF64}( reduced_density_matrix(ed_dimer, soi, β) )
+    ρ_ref = Array{ComplexF64}(reduced_density_matrix(ed_dimer, soi, β))
 
-    # -- Hybridization propagator
+    # Hybridization propagator
 
-    Δ = kd.ImaginaryTimeGF(
-        (t1, t2) -> -1.0im * V^2 *
-            (kd.heaviside(t1.bpoint, t2.bpoint) - kd.fermi(ϵ_2, contour.β)) *
-            exp(-1.0im * (t1.bpoint.val - t2.bpoint.val) * ϵ_2),
-        grid, 1, kd.fermionic, true)
+    Δ = V^2 * kd.ImaginaryTimeGF(kd.DeltaDOS(ϵ_2), grid)
 
-    function reverse(g::kd.ImaginaryTimeGF)
-        g_rev = deepcopy(g)
-        τ_0, τ_β = first(g.grid), last(g.grid)
-        for τ in g.grid
-            g_rev[τ, τ_0] = g[τ_β, τ]
-        end
-        return g_rev
-    end
-
-    # -- Pseudo Particle Strong Coupling Expansion
+    # Pseudo Particle Strong Coupling Expansion
 
     if interpolate_gfs
         ip_fwd = InteractionPair(op.c_dag(1), op.c(1), SplineInterpolatedGF(Δ))
@@ -73,23 +58,16 @@ function run_dimer(nτ, orders, orders_bare, N_samples; interpolate_gfs=false)
 
     ρ_0 = full_hs_matrix(tofockbasis(ppgf.density_matrix(expansion.P0), ed), ed)
 
-    inchworm!(expansion,
-              grid,
-              orders,
-              orders_bare,
-              N_samples)
+    inchworm!(expansion, grid, orders, orders_bare, N_samples)
 
     if interpolate_gfs
         P = [ p.GF for p in expansion.P ]
-        ppgf.normalize!(P, β) # DEBUG fixme!
+        ppgf.normalize!(P, β)
         ρ_wrm = full_hs_matrix(tofockbasis(ppgf.density_matrix(P), ed), ed)
     else
-        ppgf.normalize!(expansion.P, β) # DEBUG fixme!
+        ppgf.normalize!(expansion.P, β)
         ρ_wrm = full_hs_matrix(tofockbasis(ppgf.density_matrix(expansion.P), ed), ed)
     end
-
-    #ppgf.normalize!(expansion.P, β)
-    #ρ_wrm = density_matrix(expansion.P, ed)
 
     diff = maximum(abs.(ρ_ref - ρ_wrm))
 
@@ -103,55 +81,38 @@ function run_dimer(nτ, orders, orders_bare, N_samples; interpolate_gfs=false)
     return diff
 end
 
-function run_nτ_calc(nτ::Integer, orders, N_sampless)
-
-    comm_root = 0
-    comm = MPI.COMM_WORLD
-    comm_size = MPI.Comm_size(comm)
-    comm_rank = MPI.Comm_rank(comm)
+function run_nτ_calc(nτ, orders, N_sampless)
 
     orders_bare = orders
 
     diff_0 = run_dimer(nτ, orders, orders_bare, 0, interpolate_gfs=false)
 
-    diffs = [ run_dimer(nτ, orders, orders_bare, N_samples, interpolate_gfs=false)
-              for N_samples in N_sampless ]
+    diffs = [run_dimer(nτ, orders, orders_bare, N_samples, interpolate_gfs=false)
+             for N_samples in N_sampless]
 
-    if comm_rank == comm_root
-        @show diffs
-
+    if ismaster()
         id = MD5.bytes2hex(MD5.md5(reinterpret(UInt8, diffs)))
         filename = "data_ntau_$(nτ)_md5_$(id).h5"
-
         @show filename
-        fid = h5.h5open(filename, "w")
 
-        g = h5.create_group(fid, "data")
+        h5.h5open(filename, "w") do fid
+            g = h5.create_group(fid, "data")
 
-        h5.attributes(g)["ntau"] = nτ
-        h5.attributes(g)["diff_0"] = diff_0
+            h5.attributes(g)["ntau"] = nτ
+            h5.attributes(g)["diff_0"] = diff_0
 
-        g["orders"] = collect(orders)
-        g["orders_bare"] = collect(orders_bare)
-        g["N_sampless"] = N_sampless
+            g["orders"] = collect(orders)
+            g["orders_bare"] = collect(orders_bare)
+            g["N_sampless"] = N_sampless
 
-        g["diffs"] = diffs
-
-        h5.close(fid)
+            g["diffs"] = diffs
+        end
     end
-
-
-    return
-
 end
 
-MPI.Init()
-
 #nτs = 2 .^ (4:12)
-#N_samples = 8 * 2 .^ (0:13)
-#orderss = [0:1, 0:3]
+#N_sampless = 8 * 2 .^ (0:13)
 
-#nτs = 2 .^ (4:12)
 nτs = [1024]
 N_sampless = 2 .^ (3:23)
 orderss = [0:1, 0:3]
@@ -161,8 +122,6 @@ if ismaster()
     @show N_sampless
     @show orderss
 end
-
-# exit()
 
 for orders in orderss
     for nτ in nτs
