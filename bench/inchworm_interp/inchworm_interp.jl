9648d3958395f37fd35b940ac4d6ee77f8f5a152
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-10-06 13:59:01 +0200

    [bench] Cleaned up `inchworm_interp`

--- bench/inchworm_interp/inchworm_interp.jl
+++ bench/inchworm_interp/inchworm_interp.jl
@@ -17,86 +17,39 @@ Author: Hugo U. R. Strand (2023)
 using MPI; MPI.Init()
 using HDF5; h5 = HDF5
 
-using LinearAlgebra: diag
-
-using QuadGK: quadgk
+using LinearAlgebra: diag, diagm
 
 using Keldysh; kd = Keldysh
 using KeldyshED; ked = KeldyshED; op = KeldyshED.Operators;
 
+using QInchworm.utility
 using QInchworm.ppgf: normalize!, density_matrix
 using QInchworm.expansion: Expansion, InteractionPair
 using QInchworm.inchworm: inchworm!
 using QInchworm.spline_gf: SplineInterpolatedGF
 using QInchworm.mpi: ismaster
 
-function semi_circular_g_tau(times, t, h, β)
-
-    g_out = zero(times)
-
-    function kernel(t, w)
-        if w > 0
-            return exp(-t * w) / (1 + exp(-w))
-        else
-            return exp((1 - t)*w) / (1 + exp(w))
-        end
-    end
-
-    for (i, τ) in enumerate(times)
-        I = x -> -2 / pi / t^2 * kernel(τ/β, β*x) * sqrt(x + t - h) * sqrt(t + h - x)
-        g, err = quadgk(I, -t+h, t+h; rtol=1e-12)
-        g_out[i] = g
-    end
-
-    return g_out
+function ρ_from_n_ref(n_ref)
+    return diagm([(1 - n_ref) * (1 - n_ref),
+                  n_ref * (1 - n_ref),
+                  n_ref * (1 - n_ref),
+                  n_ref * n_ref])
 end
 
-function ρ_from_n_ref(ρ_wrm, n_ref)
-    ρ_ref = zero(ρ_wrm)
-    ρ_ref[1, 1] = (1 - n_ref) * (1 - n_ref)
-    ρ_ref[2, 2] = n_ref * (1 - n_ref)
-    ρ_ref[3, 3] = n_ref * (1 - n_ref)
-    ρ_ref[4, 4] = n_ref * n_ref
-    return ρ_ref
-end
-
-function ρ_from_ρ_ref(ρ_wrm, ρ_ref)
-    ρ = zero(ρ_wrm)
-    ρ[1, 1] = ρ_ref[1]
-    ρ[2, 2] = ρ_ref[2]
-    ρ[3, 3] = ρ_ref[3]
-    ρ[4, 4] = ρ_ref[4]
-    return ρ
-end
-
-function get_ρ_exact(ρ_wrm)
-    n = 0.5460872495307262
-    return ρ_from_n_ref(ρ_wrm, n)
-end
-
-function get_ρ_nca(ρ_wrm)
-    rho_nca = [0.1961713995875524, 0.2474226001525296, 0.2474226001525296, 0.3089834001073883]
-    return ρ_from_ρ_ref(ρ_wrm , rho_nca)
-end
-
-function get_ρ_oca(ρ_wrm)
-    rho_oca = [0.2018070389569783, 0.2476929924482211, 0.2476929924482211, 0.3028069761465793]
-    return ρ_from_ρ_ref(ρ_wrm , rho_oca)
-end
-
-function get_ρ_tca(ρ_wrm)
-    rho_tca = [0.205163794520457, 0.2478638876741985, 0.2478638876741985, 0.2991084301311462]
-    return ρ_from_ρ_ref(ρ_wrm , rho_tca)
-end
-
-function reverse(g::kd.ImaginaryTimeGF)
-    g_rev = deepcopy(g)
-    τ_0, τ_β = first(g.grid), last(g.grid)
-    for τ in g.grid
-        g_rev[τ, τ_0] = g[τ_β, τ]
-    end
-    return g_rev
-end
+# Reference data from DLR
+const ρ_exa = ρ_from_n_ref(0.5460872495307262)
+const ρ_nca = diagm([0.1961713995875524,
+                     0.2474226001525296,
+                     0.2474226001525296,
+                     0.3089834001073883])
+const ρ_oca = diagm([0.2018070389569783,
+                     0.2476929924482211,
+                     0.2476929924482211,
+                     0.3028069761465793])
+const ρ_tca = diagm([0.205163794520457,
+                     0.2478638876741985,
+                     0.2478638876741985,
+                     0.2991084301311462])
 
 function run_hubbard_dimer(nτ, orders, orders_bare, N_samples, μ_bethe, interpolation)
 
@@ -105,31 +58,23 @@ function run_hubbard_dimer(nτ, orders, orders_bare, N_samples, μ_bethe, interp
     μ = 0.0
     t_bethe = 1.0
 
-    # -- ED solution
+    # ED solution
 
     H_imp = -μ * (op.n(1) + op.n(2))
 
-    # -- Impurity problem
+    # Impurity problem
 
-    contour = kd.ImaginaryContour(β=β);
-    grid = kd.ImaginaryTimeGrid(contour, nτ);
+    contour = kd.ImaginaryContour(β=β)
+    grid = kd.ImaginaryTimeGrid(contour, nτ)
 
     soi = ked.Hilbert.SetOfIndices([[1], [2]])
     ed = ked.EDCore(H_imp, soi)
 
-    # -- Hybridization propagator
+    # Hybridization propagator
 
-    tau = [ real(im * τ.bpoint.val) for τ in grid ]
-    delta_bethe = V^2 * semi_circular_g_tau(tau, t_bethe, μ_bethe, β)
+    Δ = V^2 * kd.ImaginaryTimeGF(kd.bethe_dos(ϵ=μ_bethe, t=0.5), grid)
 
-    Δ = kd.ImaginaryTimeGF(
-        (t1, t2) -> 1.0im * V^2 *
-            semi_circular_g_tau(
-                [-imag(t1.bpoint.val - t2.bpoint.val)],
-                t_bethe, μ_bethe, β)[1],
-        grid, 1, kd.fermionic, true)
-
-    # -- Pseudo Particle Strong Coupling Expansion
+    # Pseudo Particle Strong Coupling Expansion
 
     if interpolation
         ip_1_fwd = InteractionPair(op.c_dag(1), op.c(1), SplineInterpolatedGF(Δ))
@@ -163,58 +108,29 @@ function run_hubbard_dimer(nτ, orders, orders_bare, N_samples, μ_bethe, interp
         ρ_wrm = full_hs_matrix(tofockbasis(density_matrix(expansion.P), ed), ed)
     end
 
-    ρ_exa = get_ρ_exact(ρ_wrm)
-    ρ_nca = get_ρ_nca(ρ_wrm)
-    ρ_oca = get_ρ_oca(ρ_wrm)
-    ρ_tca = get_ρ_tca(ρ_wrm)
+    global ρ_nca, ρ_oca, ρ_tca, ρ_exa
 
     diff_nca = maximum(abs.(ρ_wrm - ρ_nca))
     diff_oca = maximum(abs.(ρ_wrm - ρ_oca))
     diff_tca = maximum(abs.(ρ_wrm - ρ_tca))
     diff_exa = maximum(abs.(ρ_wrm - ρ_exa))
 
-    ρ_000 = real(diag(ρ_0))
-    ρ_exa = real(diag(ρ_exa))
-    ρ_nca = real(diag(ρ_nca))
-    ρ_oca = real(diag(ρ_oca))
-    ρ_tca = real(diag(ρ_tca))
-    ρ_wrm = real(diag(ρ_wrm))
-
-    if comm_rank == 0
-        @show ρ_wrm
-        @show sum(ρ_wrm)
-
-        @show ρ_000
-        @show ρ_nca
-        @show ρ_oca
-        @show ρ_tca
-        @show ρ_exa
-
-        @show diff_nca
-        @show diff_oca
-        @show diff_tca
-        @show diff_exa
-    end
-
-    ρ_ref = [ρ_000, ρ_nca, ρ_oca, ρ_tca, ρ_exa]
+    ρ_ref = real.(diag.([ρ_0, ρ_nca, ρ_oca, ρ_tca, ρ_exa]))
     diffs = [diff_nca, diff_oca, diff_tca, diff_exa]
 
-    return ρ_wrm, ρ_ref, diffs
+    return real(diag(ρ_wrm)), ρ_ref, diffs
 end
-
-comm = MPI.COMM_WORLD
-comm_size = MPI.Comm_size(comm)
-comm_rank = MPI.Comm_rank(comm)
 
 μ_bethe = 0.25
 N_samples = 2^16
 nτ_list = 2 .^ (3:10)
 
-if comm_rank == 0
-    fid = h5.h5open("data.h5", "w")
-    data_g = h5.create_group(fid, "data")
-    h5.attributes(data_g)["mu_bethe"] = μ_bethe
-    h5.attributes(data_g)["N_samples"] = N_samples
+if ismaster()
+    h5.h5open("data.h5", "w") do fid
+        g = h5.create_group(fid, "data")
+        h5.attributes(g)["mu_bethe"] = μ_bethe
+        h5.attributes(g)["N_samples"] = N_samples
+    end
 end
 
 for orders in [0:1, 0:2, 0:3]
@@ -231,21 +147,19 @@ for orders in [0:1, 0:2, 0:3]
         diffs_interp[i, :] = diffs
     end
 
-    if comm_rank == 0
+    if ismaster()
         println("orders = ", orders)
         print("diffs_non_interp = ")
         display(diffs_non_interp)
         print("diffs_interp = ")
         display(diffs_interp)
 
-        g = h5.create_group(data_g, "orders_$orders")
-        h5.attributes(g)["ntau_list"] = nτ_list
-        g["orders"] = collect(orders)
-        g["diffs_non_interp"] = diffs_non_interp
-        g["diffs_interp"] = diffs_interp
+        h5.h5open("data.h5", "cw") do fid
+            g = h5.create_group(fid["data"], "orders_$orders")
+            h5.attributes(g)["ntau_list"] = nτ_list
+            g["orders"] = collect(orders)
+            g["diffs_non_interp"] = diffs_non_interp
+            g["diffs_interp"] = diffs_interp
+        end
     end
-end
-
-if comm_rank == 0
-    h5.close(fid)
 end
--- bench/inchworm_interp/plot.jl
+++ bench/inchworm_interp/plot.jl
@@ -23,10 +23,10 @@ for g in data_g
     for (i, name) in enumerate(["NCA", "OCA", "2CA", "exact"])
         plt.loglog(nτ_list, diffs_non_interp[:, i], base=2,
             lw = 0.5, ls="--", color=cmap(i),
-            label = "diff(\$ρ_{$name}\$), linear interp.")
+            label = "diff(\$\\rho_{$name}\$), linear interp.")
         plt.loglog(nτ_list, diffs_interp[:, i], base=2,
             lw = 0.5, color=cmap(i),
-            label = "diff(\$ρ_{$name}\$), quadratic interp.")
+            label = "diff(\$\\rho_{$name}\$), quadratic interp.")
     end
     plt.xscale("log", base = 2)
     plt.yscale("log", base = 10)
