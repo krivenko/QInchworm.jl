48b608e09086e7f03ee0e44b105743961e42aade
Hugo U. R. Strand <hugo.strand@gmail.com>
2023-10-10 16:35:57 +0200

    [bench] plot tweaks

--- bench/bethe/plot_bethe_cthyb.py
+++ bench/bethe/plot_bethe_cthyb.py
@@ -1,3 +1,4 @@
+import glob
 import numpy as np
 
 from h5 import HDFArchive
@@ -39,16 +40,53 @@ if __name__ == "__main__":
     print(f'pto_hist_wrm = {pto_hist_wrm}')
     #exit()
     
+    def load_h5(filename):
+        print(f'--> Loading: {filename}')
+        with HDFArchive(filename, 'r') as a:
+            d = a['data']
+            diffs = list(d['diffs'])
+            N_sampless = list(d['N_sampless'])
+        return diffs, N_sampless
+
+    def load_h5_many(filenames):
+        diffs = []
+        N_sampless = []
+        for filename in filenames:
+            d, N = load_h5(filename)
+            diffs = diffs + d
+            N_sampless = N_sampless + N
+
+        diffs = np.array(diffs)
+        N_sampless = np.array(N_sampless)
+        sidx = np.argsort(N_sampless)
+        diffs, N_sampless = diffs[sidx], N_sampless[sidx]
+        return diffs, N_sampless
+        
+
     #filename = 'data_bethe_ntau_1024_maxorder_1_md5_68e17e11280c9c0cb659042ff7a8ef7d.h5'
     #filename = 'data_bethe_ntau_2048_maxorder_1_md5_1bcc2a4d808c1b5e5e924af06a0afb72.h5'
     #filename = 'data_bethe_ntau_4096_maxorder_1_md5_d3e0ae1f4957e91a05861c96af3f3162.h5'
-    filename = 'data_bethe_ntau_8192_maxorder_4_md5_05bd87e98134cebe65b400aac70dba3a.h5'
-    print(f'--> Loading: {filename}')
-    with HDFArchive(filename, 'r') as a:
-        d = a['data']
-        diffs = d['diffs']
-        N_sampless = d['N_sampless']
+    #filename = 'data_bethe_ntau_8192_maxorder_4_md5_05bd87e98134cebe65b400aac70dba3a.h5'
 
+    filenames = [
+        'data_bethe_ntau_32768_maxorder_4_md5_8dad40a36de8a1d8e07f214f49181111.h5',
+        #'data_bethe_ntau_32768_maxorder_4_md5_e189735fc6a1f5998ff262ef40bd0375.h5',
+        'data_bethe_ntau_32768_maxorder_4_md5_2477a72ab93e825adfd33430b4fda00e.h5',
+        'data_bethe_ntau_32768_maxorder_4_md5_ad03061e692df3602402d5c64de58e52.h5',
+        'data_bethe_ntau_32768_maxorder_4_md5_2431dbcb83245516e733a5aa93b9527b.h5',
+        #'data_bethe_ntau_32768_maxorder_4_md5_0349c62c31515d2ed899a01112f80f2e.h5',
+        #'data_bethe_ntau_32768_maxorder_4_md5_2c180063b42ef9dc9948120545106599.h5',
+        ]
+
+
+    #filenames = ['data_bethe_ntau_8192_maxorder_4_md5_05bd87e98134cebe65b400aac70dba3a.h5']
+
+    diffs, N_sampless = load_h5_many(filenames)
+
+    print(diffs)
+    print(N_sampless)
+    print(len(diffs))
+    
     filename = 'data_bethe_cthyb.h5'
     print(f'--> Loading: {filename}')
     with HDFArchive(filename, 'r') as a:
@@ -105,8 +143,8 @@ if __name__ == "__main__":
         x = N_sampless
         y = diffs
 
-        n_cut = 5
-        x, y = x[:n_cut], y[:n_cut]
+        n_cut = 12
+        x, y = x[n_cut:], y[n_cut:]
         
         return np.sum(np.abs(y/(C/x) - 1.))
 
@@ -114,30 +152,35 @@ if __name__ == "__main__":
     res = minimize(target_function, 0.1)
     print(res)
 
-    res_qmc = minimize(target_function_qmc, 0.01)
+    res_qmc = minimize(target_function_qmc, 0.001)
     print(res_qmc)
     
     import matplotlib.pyplot as plt
 
-    plt.figure(figsize=(3.25, 2.5))
+    plt.figure(figsize=(3.25, 2.))
 
-    gs = GridSpec(
-        2, 1,
-        width_ratios=[1],
-        height_ratios=[1, 0.3],
-        wspace=0.0, hspace=0.45,
-        bottom=0.14, top=0.99,
-        left=0.18, right=0.98,
-        )
-    
+    if False:
+        gs = GridSpec(
+            2, 1,
+            width_ratios=[1],
+            height_ratios=[1, 0.3],
+            wspace=0.0, hspace=0.45,
+            bottom=0.20, top=0.99,
+            left=0.18, right=0.98,
+            )
+    else:
+        gs = GridSpec(
+            1, 1,
+            width_ratios=[1],
+            height_ratios=[1],
+            wspace=0.0, hspace=0.45,
+            bottom=0.14, top=0.99,
+            left=0.16, right=0.99,
+            )
+        
     #subp = [2, 1, 1]
     #plt.subplot(*subp); subp[-1] += 1
-    plt.subplot(gs[0, 0])
-
-    N = np.logspace(0.2, 6.5, num=10)
-    plt.plot(N, res_qmc.x / N, ':k', lw=0.5,
-             #label=r'$1/N$'
-             )
+    #plt.subplot(gs[0, 0])
 
     #x = np.logspace(0, 8, num=10)
     #plt.plot(x, res.x/np.sqrt(x), '--k', lw=0.5, label=r'$\sim 1/\sqrt{N}$')
@@ -146,13 +189,20 @@ if __name__ == "__main__":
     plt.plot(N, res.x / np.sqrt(N), '--k', lw=0.5,
              #label=r'$1/\sqrt{N}$',
              )
-    
-    plt.plot(N_sampless, diffs, '.-', alpha=0.75, label='Quasi Monte Carlo')
 
-    l = plt.plot(ds.n_samples, errs, '.', alpha=0.02)
+    label = '2'
+    if label == '2':
+        plt.plot(N_sampless, diffs, 'o-', markersize=2.5, alpha=0.75, label='Quasi Monte Carlo')
+        plt.text(2e7, 5e-8, r'$\propto 1/N$', ha='center', va='top')
+        N = np.logspace(0.2, 6.5, num=10)
+        plt.plot(N, res_qmc.x / N, ':k', lw=0.5)
+    else:
+        plt.plot([], [], 'o-', markersize=2.5, alpha=0.75, label='Quasi Monte Carlo')
+
+    l = plt.plot(ds.n_samples, errs, 's', alpha=0.02, markersize=2.5)
     color = l[0].get_color()
     plt.plot(n_samples, err_avgs, '-', color=color, alpha=0.75)
-    plt.plot([], [], '.-', color=color, alpha=0.75, label='Monte Carlo')
+    plt.plot([], [], 's-', markersize=2.5, color=color, alpha=0.75, label='Monte Carlo')
 
     #plt.plot(2e9, 1e-3, 'o', color='white')
     #plt.plot(2e9, 1e2, 'o', color='white')
@@ -160,64 +210,67 @@ if __name__ == "__main__":
     #plt.plot(1, 1e-8, 'o', color='red')
     #plt.plot(1e13, 1e-8, 'o', color='red')
     plt.text(1e8, 2e-4, r'$\propto 1/\sqrt{N}$', ha='left', va='top')
-    plt.text(2e7, 5e-8, r'$\propto 1/N$', ha='center', va='top')
     
     plt.loglog([],[])
     #plt.semilogx([], [])
-    #plt.grid(True)
+    plt.grid(True)
     plt.xlabel(r'Samples $N$')
-    plt.ylabel(r'Error in  $\rho$')
+    #plt.ylabel(r'Error in  $\rho$')
+    plt.ylabel(r'$|\rho - \rho_{exact}|$')
     plt.legend(loc='best')
     #plt.ylim([1e-4, 1e1])
     #plt.xlim([1e-2, 1e9])
 
-    #plt.axis('equal')
-    #plt.ylim([1e-5, 1e0])
-    plt.xlim(left=1, right=1e15)
-    plt.ylim(bottom=1e-8)
+    plt.axis('equal')
+    plt.ylim([1e-8, 1e0])
+    #plt.xlim(left=1, right=1e15)
+    #plt.ylim(bottom=1e-8)
 
-    #plt.subplot(*subp); subp[-1] += 1
-    plt.subplot(gs[1, 0])
+    if False:
+        # ===============================================
+        #plt.subplot(*subp); subp[-1] += 1
+        plt.subplot(gs[1, 0])
+        # ===============================================
 
-    p = ps[-1]
-    #print(dir(p))
-    #print(p.perturbation_order_total)
+        p = ps[-1]
+        #print(dir(p))
+        #print(p.perturbation_order_total)
 
 
-    n = 0
-    pto = np.zeros_like(ds.objects[-1].pto)
-    for d in ds:
-        if d.n_samples == n_samples[-1]:
-            pto += d.pto
-            n += 1
-    pto /= n
-        
-    #pto = p.perturbation_order_total
-    #pto = pto.data[:20]
-    
-    #print(pto.shape)
-    #print(pto)
-    #print(dir(pto))
-    #import matplotlib.pyplot as plt
-    x = np.arange(0, len(pto))
-    pto /= np.sum(pto)
+        n = 0
+        pto = np.zeros_like(ds.objects[-1].pto)
+        for d in ds:
+            if d.n_samples == n_samples[-1]:
+                pto += d.pto
+                n += 1
+        pto /= n
 
-    color_wrm = plt.plot([], [])[0].get_color()
-    color_cth = plt.plot([], [])[0].get_color()
+        #pto = p.perturbation_order_total
+        #pto = pto.data[:20]
 
-    plt.bar(x, pto, alpha=0.75, color=color_cth)
-    pto_hist_wrm = np.abs(pto_hist_wrm)
-    pto_hist_wrm /= np.sum(pto_hist_wrm)
-    plt.bar(range(len(pto_hist_wrm)), pto_hist_wrm, alpha=0.75, color=color_wrm)
+        #print(pto.shape)
+        #print(pto)
+        #print(dir(pto))
+        #import matplotlib.pyplot as plt
+        x = np.arange(0, len(pto))
+        pto /= np.sum(pto)
 
-    plt.semilogy([], [])
-    #plt.ylim(top=1)
-    plt.xlabel('Perturbation order')
-    plt.ylabel('Probabilbity')
-    plt.xticks(np.arange(0, pto_max), labels=[f'{i:d}' for i in range(0, pto_max)])
-    plt.ylim(bottom=5e-8, top=10)
-    plt.yticks([1e-6, 1e-3, 1e0])
+        color_wrm = plt.plot([], [])[0].get_color()
+        color_cth = plt.plot([], [])[0].get_color()
+
+        plt.bar(x, pto, alpha=0.75, color=color_cth)
+        pto_hist_wrm = np.abs(pto_hist_wrm)
+        pto_hist_wrm /= np.sum(pto_hist_wrm)
+        plt.bar(range(len(pto_hist_wrm)), pto_hist_wrm, alpha=0.75, color=color_wrm)
+
+        plt.semilogy([], [])
+        #plt.ylim(top=1)
+        plt.xlabel('Perturbation order')
+        plt.ylabel('Probabilbity')
+        plt.xticks(np.arange(0, pto_max), labels=[f'{i:d}' for i in range(0, pto_max)])
+        plt.ylim(bottom=5e-8, top=10)
+        plt.yticks([1e-6, 1e-3, 1e0])
     
     plt.tight_layout()
-    plt.savefig('figure_mc_convergence.pdf')
-    #plt.show()
+    plt.savefig(f'figure_mc_convergence_{label}.pdf')
+    plt.show()
--- bench/bethe_gf_convergence/plot_convergence.py
+++ bench/bethe_gf_convergence/plot_convergence.py
@@ -8,6 +8,20 @@ import pandas as pd
 
 from triqs_tprf.ParameterCollection import ParameterCollection
 
+
+SMALL_SIZE = 7
+MEDIUM_SIZE = 8
+BIGGER_SIZE = 8
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
 def read_and_sort(filenames):
 
     ps = []
@@ -34,9 +48,9 @@ def read_and_sort(filenames):
         p = ParameterCollection(**d)
         ps.append(p)
 
-        d.pop('gf')
-        d.pop('gf_ref')
-        d.pop('tau')
+        #d.pop('gf')
+        #d.pop('gf_ref')
+        #d.pop('tau')
         
         d.pop('orders')
         d.pop('orders_bare')
@@ -67,43 +81,268 @@ paths = [
     #'./n_pts_after_max_1/data*h5',
     #'./n_pts_after_max_NoLimit/data*h5',
     './*h5',
+    #'./*ntau_1024*h5',
     ]
-filenames = glob.glob(paths[0])
+#filenames = glob.glob(paths[0])
+
+filenames = \
+    glob.glob('data_order_0:1_ntau_256*.h5') + \
+    glob.glob('data_order_0:2_ntau_128*.h5') + \
+    glob.glob('data_order_0:3_ntau_256*.h5') + \
+    glob.glob('data_order_0:4_ntau_256*.h5') + \
+    glob.glob('data_order_0:5_ntau_1024*.h5') + \
+    glob.glob('data_order_0:6_ntau_1024*.h5') + \
+    glob.glob('data_order_0:7_ntau_1024*.h5') 
+    
 ps, df = read_and_sort(filenames)
 
-plt.figure(figsize=(3.25*2, 5))
+plt.figure(figsize=(3.25, 3.5))
 
+from matplotlib.gridspec import GridSpec
+gs = GridSpec(
+    2, 2,
+    width_ratios=[1.1, 1.6],
+    height_ratios=[1.2, 2.0],
+    wspace=0.0, hspace=0.15,
+    bottom=0.11, top=0.99,
+    left=0.15, right=0.99,
+    )
+
+#subp = [2, 1, 1]
+#plt.subplot(*subp); subp[-1] += 1
+#subp = [2, 2, 3]
+
+ax = plt.subplot(gs[1, 1])
+
+plt.text(0.9, 0.9, "(c)", size=9, ha="center", va="center", transform=ax.transAxes)
+    
 ntaus = np.sort(np.unique(df['ntau']))
 orders = np.sort(np.unique(df['order']))
 
 styles = ['o', 's', 'd', '>', '<', '^', 'x', '+']
+#alphas = [0.1]*6 + [0.5]
+#alphas = [0.75]*len(styles)
+
+alphas = np.linspace(0.2, 0.75, num=len(ntaus))
+alphas[:] = 0.75
 
 colors = []
-for ntau in ntaus:
-    c = plt.plot([], [], label=f'ntau {ntau}')[0].get_color()
-    colors.append(c)
 
-for style, order in zip(styles, orders):
-    plt.plot([], [], style, color='gray', label=f'order {order}')
-    
-for style, order in zip(styles, orders):
-    for color, ntau in zip(colors, ntaus):
+from matplotlib.cm import get_cmap
+cmap = get_cmap(name='brg_r')
+#cmap = get_cmap(name='jet')
+#cmap = get_cmap(name='rainbow')
+
+if False:
+    for idx, ntau in enumerate(ntaus):
+        #c = plt.plot([], [], label=f'ntau {ntau}')[0].get_color()
+        i = np.linspace(0, 1, num=len(ntaus))[idx]
+        c = cmap(i)
+        plt.plot([], [], color=c, label=f'ntau {ntau}')
+        colors.append(c)
+        
+    for style, order in zip(styles, orders):
+        plt.plot([], [], style, color='gray', label=f'order {order}')
+else:
+    for idx, order in enumerate(orders):
+        #c = plt.plot([], [], label=f'ntau {ntau}')[0].get_color()
+        i = np.linspace(0, 1, num=len(orders))[idx]
+        c = cmap(i)
+        #plt.plot([], [], color=c, label=f'order {order}')
+        colors.append(c)
+
+    if False:
+        for style, alpha, ntau in zip(styles, alphas, ntaus):
+            e = int(np.log2(ntau))
+            plt.plot([], [], '-' + style, color='k',
+                     #label=r'$N_\tau =' + f'{ntau}$',
+                     label=r'$N_\tau = 2^{' + f'{e}' + r'}$',
+                     markersize=4, alpha=alpha)
+
+        
+#for style, alpha, order in zip(styles, alphas, orders):
+#    for color, ntau in zip(colors, ntaus):
+for color, order in zip(colors, orders):
+    for style, alpha, ntau in zip(styles, alphas, ntaus):
         dfc = df[ df['ntau'] == ntau ]
         dfc = dfc[ dfc['order'] == order ]
         dfc = dfc.sort_values(by='N_samples')
         
         plt.plot(
             dfc['N_samples'], dfc['error'],
-            '-' + style, alpha=0.5, color=color,
-            #label=f'ntau {ntau}, order {order}',
+            '-', alpha=alpha, color=color, markersize=4,
             )
 
-plt.legend()
-plt.loglog([], [])
-plt.grid(True)
-#plt.ylim(bottom=1e-4)
+        N = np.array(dfc['N_samples'])
+        e = np.array(dfc['error'])
+        #print(type(e))
+        if len(e) > 0:
+            plt.plot(
+                N[-1], e[-1],
+                #style,
+                'o',
+                alpha=alpha, color=color,
+                #markersize=4,
+                )
 
-plt.show(); exit()
+
+import matplotlib as mpl
+gc = mpl.rcParams['grid.color']
+opts = dict(color=gc, alpha=1.0, lw=1.0, zorder=-10)
+
+x = np.logspace(1.5, 4.5, num=10)
+plt.plot(x, 1/x, '--', **opts)
+
+x = np.logspace(2.5, 5.5, num=10)
+plt.plot(x, 100/x, '--', label='$\propto 1/N_{qMC}$', **opts)
+            
+plt.legend(
+    fontsize=7, ncol=2, loc='lower left',
+    labelspacing=0.1,
+    columnspacing=0.75,
+    )
+plt.loglog([], [])
+#plt.axis('equal')
+#plt.xlim([1e1, 1e7])
+plt.grid(True)
+#plt.xticks([1e2, 1e4, 1e6])
+plt.xticks([1e2, 1e3, 1e4, 1e5])
+
+plt.tick_params(labelleft=False, left=False)
+
+plt.xlabel('$N_{qMC}$')
+plt.ylabel(r'$|G(\beta/2) - G_{exact}(\beta/2)|$')
+
+# -- Best convergence plot
+
+errors = []
+for order in orders:
+    dfc = df[ df['order'] == order ]
+    dfc = dfc[ dfc['ntau'] == np.max(dfc['ntau']) ]
+    dfc = dfc[ dfc['N_samples'] == np.max(dfc['N_samples']) ]
+    errors.append(dfc['error'][0])
+
+# -- BEGIN DATA
+
+# A Tensor Train Continuous Time Solver for Quantum Impurity Models
+# https://arxiv.org/abs/2303.11199
+# figure 3
+
+from io import StringIO
+data = np.loadtxt(StringIO("""0.21660649819494537, 0.4016485041281371
+2.0036101083032483, 0.23608963626703663
+4.061371841155235, 0.12814223889440857
+6.010830324909748, 0.06772815277441427
+8.014440433212997, 0.033054513475732496
+10.072202166064983, 0.014505692324279153
+12.075812274368232, 0.005723903557562126
+14.079422382671481, 0.0019258186341850783
+16.028880866425993, 0.0005379838403443692
+18.08664259927798, 0.00012151094113758904
+20.03610108303249, 0.000024030855432419302
+22.03971119133574, 0.0000037417685733024595
+24.09747292418773, 4.349722373637536e-7
+26.046931407942242, 6.089973244419447e-8"""), delimiter=',')
+o, e = data.T
+o = np.round(o)
+ttd = dict(orders=o, errors=e)
+
+# -- END DATA
+
+#plt.figure()
+#plt.subplot(*subp); subp[-1] += 1
+
+plt.subplot(gs[1, 0], sharey=ax)
+
+plt.text(-0.085, 0.9, "(b)", size=9, ha="center", va="center", transform=ax.transAxes)
+
+l = plt.plot(orders, errors, '-k',
+             #label='bold (qMC)',
+             alpha=0.75)
+
+color = l[0].get_color()
+#plt.plot([], [], 'o-', label='Bold (qMC)', alpha=0.75, color=color)
+plt.plot([], [], 'o-', label='Inchworm', alpha=0.75, color=color, markerfacecolor='gray', markeredgecolor='gray')
+
+plt.plot([], [], '-s',
+         label='Bare (TTD)', color='k', markerfacecolor='gray', markeredgecolor='gray', alpha=0.75)
+
+n = 12
+plt.plot(ttd['orders'][:n], ttd['errors'][:n], '-',
+         #label='Bare (TTD)',
+         color='k', alpha=0.75)
+
+m = [0] + list(range(4, 12))
+plt.plot(ttd['orders'][m], ttd['errors'][m], 's',
+         color='gray', alpha=0.75)
+
+for color, order in zip(colors, orders):
+    idx = list(orders).index(order)
+    plt.plot(orders[idx], errors[idx], 'o', color=color, alpha=0.75)
+
+for color, order in zip(colors, orders):
+    if order in set([2,4,6]):
+        idx = list(ttd['orders']).index(order)
+        plt.plot(ttd['orders'][idx], ttd['errors'][idx], 's',
+                 color=color, alpha=0.75)
+
+
+plt.legend(loc='lower left', fontsize=7)
+plt.semilogy([], [])
+
+#plt.ylim(bottom=1e-8)
+#plt.ylim([1e-8, 1e0])
+
+#plt.xlim([0, 30])
+plt.xlim([0, 20])
+plt.xticks([0, 5, 10, 15])
+plt.ylim(bottom=1e-5)
+
+plt.grid(True)
+plt.xlabel('Order')
+#plt.ylabel(r'$|G(\beta/2) - G_{exact}(\beta/2)|$')
+plt.ylabel(r'$\max_{\tau}|G(\tau) - G_{exact}(\tau)|$', labelpad=-2)
+
+
+#plt.subplot(*subp); subp[-1] += 1
+
+plt.subplot(gs[0, :])
+
+plt.text(-0.53, 1.18, "(a)", size=9, ha="center", va="center", transform=ax.transAxes)
+
+for color, order in zip(colors, orders):
+    dfc = df[ df['order'] == order ]
+    dfc = dfc[ dfc['ntau'] == np.max(dfc['ntau']) ]
+    dfc = dfc[ dfc['N_samples'] == np.max(dfc['N_samples']) ]
+
+    d = dfc
+    plt.plot(d['tau'][0], d['gf'][0].imag,
+             color=color, alpha=0.75, label=f'Order {order}')
+
+plt.plot(d['tau'][0], d['gf_ref'][0].imag, ':', color='y', alpha=1., label='Exact')
+
+plt.legend(loc='best', fontsize=7, ncol=2)
+plt.ylim(bottom=0)
+plt.grid(True)
+plt.xlabel(r'$\tau$', labelpad=-10)
+plt.ylabel(r'$G(\tau)$')
+#exit()
+
+#plt.tight_layout()
+plt.savefig('figure_gf_convergence.pdf')
+#plt.show();
+exit()
+
+
+
+
+
+
+
+
+
+
+
 
 plt.figure(figsize=(3.25, 5))
 subp = [2, 1, 1]
