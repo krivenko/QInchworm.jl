bb7c68ac459acbe7b19b55586fee9a3c5530efd2
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-10-10 13:41:40 +0200

    [bench] Cleaned up `bethe_gf_convergence`
    `plot_convergence.py` has been translated from Python to Julia.

--- bench/bethe_gf_convergence/bethe_gf_convergence.jl
+++ bench/bethe_gf_convergence/bethe_gf_convergence.jl
@@ -4,47 +4,19 @@ Author: Hugo U. R. Strand (2023)
 
 """
 
-using MD5
+using MPI; MPI.Init()
 using HDF5; h5 = HDF5
 
-using Test
-using LinearInterpolations: Interpolate
-
-using MPI; MPI.Init()
-
-import PyPlot; plt = PyPlot
-
-using LinearAlgebra: diag
-using QuadGK: quadgk
+using MD5
+using ArgParse
 
 using Keldysh; kd = Keldysh
-using KeldyshED; ked = KeldyshED; op = KeldyshED.Operators;
+using KeldyshED; ked = KeldyshED; op = KeldyshED.Operators
 
-using QInchworm.ppgf: normalize!, density_matrix
+using QInchworm.ppgf: normalize!
 using QInchworm.expansion: Expansion, InteractionPair, add_corr_operators!
-using QInchworm.inchworm: inchworm_matsubara!, correlator_2p
+using QInchworm.inchworm: inchworm!, correlator_2p
 using QInchworm.mpi: ismaster
-
-function semi_circular_g_tau(times, t, h, β)
-
-    g_out = zero(times)
-
-    function kernel(t, w)
-        if w > 0
-            return exp(-t * w) / (1 + exp(-w))
-        else
-            return exp((1 - t)*w) / (1 + exp(w))
-        end
-    end
-
-    for (i, τ) in enumerate(times)
-        I = x -> -2 / pi / t^2 * kernel(τ/β, β*x) * sqrt(x + t - h) * sqrt(t + h - x)
-        g, err = quadgk(I, -t+h, t+h; rtol=1e-12)
-        g_out[i] = g
-    end
-
-    return g_out
-end
 
 function run_bethe(nτ, orders, orders_bare, orders_gf, N_samples, n_pts_after_max)
 
@@ -54,175 +26,89 @@ function run_bethe(nτ, orders, orders_bare, orders_gf, N_samples, n_pts_after_m
     V = 0.5 * t_bethe
     μ_bethe = 0.0
 
-    # -- ED solution
+    # ED solution
 
     H_imp = -μ * op.n(1)
 
-    # -- Impurity problem
+    # Impurity problem
 
-    contour = kd.ImaginaryContour(β=β);
-    grid = kd.ImaginaryTimeGrid(contour, nτ);
+    contour = kd.ImaginaryContour(β=β)
+    grid = kd.ImaginaryTimeGrid(contour, nτ)
 
     soi = ked.Hilbert.SetOfIndices([[1]])
     ed = ked.EDCore(H_imp, soi)
 
-    # -- Hybridization propagator
+    # Hybridization propagator
 
-    tau = [ real(im * τ.bpoint.val) for τ in grid ]
-    delta_bethe = V^2 * semi_circular_g_tau(tau, t_bethe, μ_bethe, β)
+    Δ = V^2 * kd.ImaginaryTimeGF(kd.bethe_dos(t=t_bethe/2, ϵ=μ_bethe), grid)
 
-    Δ = kd.ImaginaryTimeGF(
-        (t1, t2) -> 1.0im * V^2 *
-            semi_circular_g_tau(
-                [-imag(t1.bpoint.val - t2.bpoint.val)],
-                t_bethe, μ_bethe, β)[1],
-        grid, 1, kd.fermionic, true)
-
-    function reverse(g::kd.ImaginaryTimeGF)
-        g_rev = deepcopy(g)
-        τ_0, τ_β = first(g.grid), last(g.grid)
-        for τ in g.grid
-            g_rev[τ, τ_0] = g[τ_β, τ]
-        end
-        return g_rev
-    end
-
-    # -- Pseudo Particle Strong Coupling Expansion
+    # Pseudo Particle Strong Coupling Expansion
 
     ip_1_fwd = InteractionPair(op.c_dag(1), op.c(1), Δ)
     ip_1_bwd = InteractionPair(op.c(1), op.c_dag(1), reverse(Δ))
     expansion = Expansion(ed, grid, [ip_1_fwd, ip_1_bwd])
 
-    inchworm_matsubara!(expansion, grid, orders, orders_bare, N_samples;
-                        n_pts_after_max=n_pts_after_max)
+    inchworm!(expansion, grid, orders, orders_bare, N_samples;
+              n_pts_after_max=n_pts_after_max)
     normalize!(expansion.P, β)
 
     add_corr_operators!(expansion, (op.c(1), op.c_dag(1)))
     g = correlator_2p(expansion, grid, orders_gf, N_samples)
 
-    # ==
     if ismaster()
         id = MD5.bytes2hex(MD5.md5(reinterpret(UInt8, vcat(g[1].mat.data...))))
         filename = "data_order_$(orders)_ntau_$(nτ)_N_samples_$(N_samples)_md5_$(id).h5"
-
         @show filename
-        fid = h5.h5open(filename, "w")
-        grp = h5.create_group(fid, "data")
 
-        h5.attributes(grp)["beta"] = β
-        h5.attributes(grp)["ntau"] = nτ
-        h5.attributes(grp)["n_pts_after_max"] = n_pts_after_max
-        h5.attributes(grp)["N_samples"] = N_samples
+        h5.h5open(filename, "w") do fid
+            grp = h5.create_group(fid, "data")
 
-        grp["orders"] = collect(orders)
-        grp["orders_bare"] = collect(orders_bare)
-        grp["orders_gf"] = collect(orders_gf)
+            h5.attributes(grp)["beta"] = β
+            h5.attributes(grp)["ntau"] = nτ
+            h5.attributes(grp)["n_pts_after_max"] = n_pts_after_max
+            h5.attributes(grp)["N_samples"] = N_samples
 
-        grp["tau"] = collect(kd.imagtimes(g[1].grid))
-        grp["gf"] = g[1].mat.data[1, 1, :]
-        grp["gf_ref"] = -Δ.mat.data[1, 1, :] / V^2
+            grp["orders"] = collect(orders)
+            grp["orders_bare"] = collect(orders_bare)
+            grp["orders_gf"] = collect(orders_gf)
 
-        h5.close(fid)
-    end
-
-    #if ismaster()
-    if false
-
-        τ = kd.imagtimes(g[1].grid)
-        τ_ref = collect(LinRange(0, β, 128))
-
-        plt.figure(figsize=(3.25*2, 8))
-        subp = [3, 2, 1]
-
-        for s in 1:length(expansion.P)
-            plt.subplot(subp...); subp[end] += 1;
-
-            x = collect(τ)
-            y = collect(imag(expansion.P[s].mat.data[1, 1, :]))
-            P_int = Interpolate(x, y)
-            P = P_int.(τ_ref)
-
-            plt.plot(τ_ref, -P, label="P$(s)")
-            plt.semilogy([], [])
-            plt.ylabel(raw"$P_\Gamma(\tau)$")
-            plt.xlabel(raw"$\tau$")
-            plt.legend(loc="best")
-
-            plt.subplot(subp...); subp[end] += 1;
-            for (o, P) in enumerate(expansion.P_orders)
-                p = imag(P[s].mat.data[1, 1, :])
-                plt.semilogy(τ, -p, label="order $(o-1) ref", alpha=0.25)
-            end
-            plt.ylim(bottom=1e-9)
-            plt.ylabel(raw"$P_\Gamma(\tau)$")
-            plt.xlabel(raw"$\tau$")
-            plt.legend(loc="best")
+            grp["tau"] = collect(kd.imagtimes(g[1].grid))
+            grp["gf"] = g[1].mat.data[1, 1, :]
+            grp["gf_ref"] = -Δ.mat.data[1, 1, :] / V^2
         end
-
-        x = collect(τ)
-        y = collect(imag(g[1].mat.data[1, 1, :]))
-        g_int = Interpolate(x, y)
-        gr = g_int.(τ_ref)
-
-        plt.subplot(subp...); subp[end] += 1;
-        plt.title("nτ = $(length(τ)), N_samples = $N_samples")
-        plt.plot(τ, imag(g[1].mat.data[1, 1, :]), "--", label="InchW")
-        plt.plot(τ, -imag(Δ.mat.data[1, 1, :])/V^2, "--", label="Bethe")
-        plt.xlabel(raw"$\tau$")
-        plt.ylabel(raw"$G_{11}(τ)$")
-        plt.legend(loc="best")
-        plt.ylim(bottom=0)
-
-        plt.tight_layout()
-        plt.savefig("figure_ntau_$(nτ)_N_samples_$(N_samples)_orders_$(orders).pdf")
-        #plt.show()
     end
 end
 
-
-
-@assert length(ARGS) == 4
-
-order = parse(Int, ARGS[1])
-nτ = parse(Int, ARGS[2])
-N_samples = parse(Int, ARGS[3])
-n_pts_after_max = parse(Int, ARGS[4])
-
-if n_pts_after_max == 0
-    n_pts_after_max = typemax(Int64)
+s = ArgParseSettings()
+@add_arg_table s begin
+    "order"
+        help = "Highest expansion order to account for"
+        arg_type = Int
+    "ntau"
+        help = "Number of imaginary time slices"
+        arg_type = Int
+    "N_samples"
+        help = "Number of qMC samples to be taken"
+        arg_type = Int
+    "--n_pts_after_max"
+        help = "Maximal number of points in the after-t_w region"
+        arg_type = Int
+        default = typemax(Int64)
 end
 
-order_gf = order - 1
+parsed_args = parse_args(ARGS, s)
+
+order = parsed_args["order"]
+nτ = parsed_args["ntau"]
+N_samples = parsed_args["N_samples"]
+n_pts_after_max = parsed_args["n_pts_after_max"]
 
 if ismaster()
-    println("order $(order) nτ $(nτ) N_samples $(N_samples) n_pts_after_max $(n_pts_after_max)")
+    println("order $(order) nτ $(nτ) N_samples $(N_samples) " *
+            "n_pts_after_max $(n_pts_after_max)")
 end
-
-#exit()
 
 orders = 0:order
-orders_gf = 0:order_gf
+orders_gf = 0:(order - 1)
 
 run_bethe(nτ, orders, orders, orders_gf, N_samples, n_pts_after_max)
-
-exit()
-
-# -- Old sweeps
-
-n_pts_after_max = 1
-#n_pts_after_max = typemax(Int64)
-
-#for o = [1, 2, 3, 4, 5]
-#for o = [4, 5]
-for o = [7]
-    #for o = [6, 7, 8]
-    orders = 0:o
-    orders_gf = 0:(o-1)
-    #for nτ = [64, 128, 256]
-    for nτ = [16]
-        #for N_samples = 32 * 2 .^ (0:10)
-        for N_samples = [8*2^0]
-            run_bethe(nτ, orders, orders, orders_gf, N_samples, n_pts_after_max)
-        end
-    end
-end
---
+++ bench/bethe_gf_convergence/plot_convergence.jl
@@ -0,0 +1,137 @@
+using HDF5; h5 = HDF5
+using DataFrames
+
+using PyPlot; plt = PyPlot
+
+function read_and_sort(filenames)
+
+    ps = []
+    df = DataFrame()
+
+    for filename in filenames
+        println("--> Loading: $filename")
+        d = Dict()
+        h5.h5open(filename, "r") do fid
+            grp = fid["data"]
+            for key in keys(h5.attributes(grp))
+                d[key] = h5.read_attribute(grp, key)
+            end
+            for key in keys(grp)
+                d[key] = h5.read(grp, key)
+            end
+        end
+
+        d["error"] = maximum(abs.(d["gf"] - d["gf_ref"]))
+        d["order"] = maximum(d["orders"])
+
+        push!(ps, copy(d))
+
+        pop!(d, "gf")
+        pop!(d, "gf_ref")
+        pop!(d, "tau")
+
+        pop!(d, "orders")
+        pop!(d, "orders_bare")
+        pop!(d, "orders_gf")
+
+        ndf = DataFrame(d)
+        df = vcat(df, ndf)
+    end
+
+    order = [maximum(p["orders"]) for p in ps]
+    sidx = sortperm(order)
+    ps = ps[sidx]
+
+    return ps, df
+end
+
+paths = [
+#    "./n_pts_after_max_1/data",
+#    "./n_pts_after_max_NoLimit/data",
+    "./"
+]
+
+filenames = filter(f -> endswith(f, ".h5"), readdir(paths[1], join=true))
+ps, df = read_and_sort(filenames)
+@show df
+
+plt.figure(figsize=(3.25 * 2, 5))
+
+nτs = sort(unique(df[:, "ntau"]))
+orders = sort(unique(df[:, "order"]))
+
+styles = ["o", "s", "d", ">", "<", "^", "x", "+"]
+
+colors = []
+for nτ in nτs
+    c = plt.plot([], [], label="\$n\\tau\$ $nτ")[1].get_color()
+    push!(colors, c)
+end
+
+for (style, order) in zip(styles, orders)
+    plt.plot([], [], style, color="gray", label="order $order")
+end
+
+for (style, order) in zip(styles, orders)
+    for (color, nτ) in zip(colors, nτs)
+        dfc = df[(df.ntau .== nτ) .& (df.order .== order), :]
+        sort!(dfc, :N_samples)
+
+        plt.plot(
+            dfc[:, "N_samples"], dfc[:, "error"],
+            "-" * style, alpha=0.5, color=color
+        )
+    end
+end
+
+plt.legend()
+plt.loglog([], [])
+plt.grid(true)
+
+plt.show()
+
+plt.figure(figsize=(3.25, 5))
+subp = [2, 1, 1]
+
+plt.subplot(subp...); subp[end] += 1
+
+for p in ps
+    order = maximum(p["orders"])
+    plt.plot(p["tau"], imag(p["gf"]), label="order $order", alpha=0.75)
+end
+
+p = ps[1]
+plt.plot(p["tau"], imag(p["gf_ref"]), "--k", label="Analytic")
+plt.ylim(bottom=0)
+plt.legend(loc="best")
+plt.xlabel(raw"$\tau$")
+plt.ylabel(raw"$G(\tau)$")
+plt.grid(true)
+
+plt.subplot(subp...); subp[end] += 1
+
+for path in paths
+    local filenames = filter(f -> endswith(f, ".h5"), readdir(path, join=true))
+    local ps, df = read_and_sort(filenames)
+
+    order = [maximum(p["orders"]) for p in ps]
+    err = [maximum(abs.(p["gf"] - p["gf_ref"])) for p in ps]
+    if ps[1]["n_pts_after_max"] == 1
+        label = "n_pts_after_max = 1"
+    else
+        label = "n_pts_after_max = unrestricted"
+    end
+
+    plt.plot(order, err, "-o", alpha=0.75, label=label)
+end
+
+plt.xlabel("order")
+plt.ylabel("GF error")
+plt.legend(loc="best")
+plt.semilogy([], [])
+plt.grid(true)
+
+plt.tight_layout()
+plt.savefig("figure_bethe_gf_convergence.pdf")
+
+plt.show()
