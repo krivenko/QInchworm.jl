2c8ab66c6285c1d8bdde3ab4e0183356b998c551
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-10-11 17:19:28 +0200

    [CI] Build and deploy documentation

--- docs/make.jl
+++ docs/make.jl
@@ -27,12 +27,13 @@ makedocs(;
     authors="Igor Krivenko <igor.s.krivenko@gmail.com>, " *
             "Hugo U. R. Strand <hugo.strand@gmail.com>, " *
             "Joseph Kleinhenz <kleinhenz.joseph@gmail.com>",
-    repo="https://github.com/krivenko/QInchworm.jl/blob/{commit}{path}#L{line}",
+    repo=Documenter.Remotes.GitHub("krivenko", "QInchworm.jl"),
     sitename="QInchworm.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         assets=String[],
     ),
+    warnonly=true,
     pages=[
         "Home" => "index.md",
         "Sub Modules" => Any[
