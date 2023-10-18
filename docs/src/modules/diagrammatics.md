48191d4c1bf50a76b72d088c63ee343ced224f07
Igor Krivenko <igor.s.krivenko@gmail.com>
2023-10-18 16:55:19 +0200

    [docs] Improve documentation of `QInchworm.diagrammatics`

--- docs/src/modules/diagrammatics.md
+++ docs/src/modules/diagrammatics.md
@@ -4,10 +4,14 @@ CurrentModule = QInchworm
 
 # [`QInchworm.diagrammatics`](@id QInchworm.diagrammatics)
 
-```@index
+```@autodocs
 Modules = [QInchworm.diagrammatics]
+Order   = [:module]
 ```
+
+## Internals
 
 ```@autodocs
 Modules = [QInchworm.diagrammatics]
+Public  = false
 ```
--- src/diagrammatics.jl
+++ src/diagrammatics.jl
@@ -17,29 +17,38 @@
 #
 # Authors: Joseph Kleinhenz, Igor Krivenko, Hugo U. R. Strand
 
+"""
+Strong coupling expansion diagrams and their topological properties.
+"""
 module diagrammatics
 
 using DocStringExtensions
 
 using Combinatorics: levicivita, doublefactorial
 
-
 const PairVector = Vector{Pair{Int,Int}}
 
 """
-$(TYPEDEF)
+    $(TYPEDEF)
 
-Datatype for diagram topology.
-A topology of order ``n`` consists of a partition of the ordered set ``s = \\{1,...,2n\\}``
-into ``n`` pairs `\\{(x(1), x(2)), ..., (x(2n-1), x(2n))\\}` where ``x`` is a permutation of ``s``.
-Diagrammatically a topology can be thought of as a set of arcs connecting vertices located at ``\\{1,...,2n\\}``.
-The parity of the topology is the sign of the permutation ``x``.
+Datatype for strong coupling diagram topology.
 
+A topology of order ``n`` consists of a partition of the tuple ``s = \\{1,...,2n\\}``
+into ``n`` pairs ``\\{(\\pi(1), \\pi(2)), ..., (\\pi(2n-1), \\pi(2n))\\}`` where ``\\pi`` is
+a permutation of ``s``.
+Diagrammatically, a topology can be thought of as a set of arcs connecting vertices located
+at ``\\{1,...,2n\\}``.
+The parity of the topology is the sign of the permutation ``\\pi``.
+
+# Fields
 $(TYPEDFIELDS)
 """
 struct Topology
+    "Topology order ``n``"
     order::Int
+    "List of pairs ``\\{(\\pi(1), \\pi(2)), ..., (\\pi(2n-1), \\pi(2n))\\}``"
     pairs::PairVector
+    "Parity of the permutation ``\\pi``"
     parity::Int
 
     function Topology(pairs::PairVector, parity::Int)
@@ -47,39 +56,57 @@ struct Topology
     end
 end
 
+"""
+    $(TYPEDSIGNATURES)
+
+Construct topology from a list of pairs.
+"""
 function Topology(pairs::PairVector)
     p = levicivita(collect(Iterators.flatten(pairs)))
     return Topology(pairs, p)
 end
 
+"""
+    $(TYPEDSIGNATURES)
+
+Check that topology is properly constructed, i.e. that its list of pairs is derived from
+a permutation of `1:2order` and that the parity of the permutation is correct.
+"""
 function Base.isvalid(t::Topology)
     perm = collect(Iterators.flatten(t.pairs))
     return ((t.parity == levicivita(perm)) && (sort!(perm) == 1:(2*t.order)))
 end
 
-function sortpair(p::Pair{T,T}) where {T}
+"""
+    $(TYPEDSIGNATURES)
+
+Given a pair, return its version with the smallest element coming first.
+"""
+function sortpair(p::Pair{T, T}) where {T}
     return p.first > p.second ? p.second => p.first : p
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Returns true if a given pair has one index <= `k` and the other index > `k`.
+Return true if a given integer pair has one element <= `k` and the other element > `k`.
 """
-function is_doubly_k_connected(p::Pair{Int,Int}, k::Int)
+function is_doubly_k_connected(p::Pair{Int, Int}, k::Int)
     return (p.first <= k && p.second > k) || (p.second <= k && p.first > k)
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Returns true if two arcs cross
+Return `true` if two arcs cross.
 
-Let ``p_1 = (a, b)``, ``p_2 = (x, y)`` represent two arcs, where without loss of generality we assume ``a < b`` and ``x < y``.
-Now consider the order the points ``\\{a, b, x, y\\}``.
-The orderings ``abxy``, ``axyb``, ``xyab`` are all non-crossing while ``axby`` and ``xayb`` are crossing.
+Let ``p_1 = (a_1, b_1)``, ``p_2 = (a_2, b_2)`` represent two arcs, where without loss of
+generality we assume ``a_1 < b_1`` and ``a_2 < b_2``.
+Now consider the order of the points ``\\{a_1, b_1, a_2, b_2\\}``.
+The orderings ``a_1 b_1 a_2 b_2``, ``a_1 a_2 b_2 b_1``, ``a_2 b_2 a_1 b_1`` are all
+non-crossing while ``a_1 a_2 b_1 b_2`` and ``a_2 a_1 b_2 b_1`` are crossing.
 """
-function iscrossing(p1::Pair{Int,Int}, p2::Pair{Int,Int})
+function iscrossing(p1::Pair{Int, Int}, p2::Pair{Int, Int})
     p1 = sortpair(p1)
     p2 = sortpair(p2)
     (p1.first < p2.first && p2.first < p1.second && p1.second < p2.second) && return true
@@ -88,10 +115,9 @@ function iscrossing(p1::Pair{Int,Int}, p2::Pair{Int,Int})
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Returns the number of crossing arcs in a topology.
-
+Return the number of crossing arcs in a topology.
 """
 function n_crossings(top::Topology)::Int
     n = 0
@@ -104,19 +130,18 @@ function n_crossings(top::Topology)::Int
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Returns the parity of the permutation matrix of the topolgy.
-
+Returns the parity of the topolgy.
 """
 function parity(top::Topology)::Int
     return top.parity
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Given a vector of 'connected' arcs and a vector of 'disconnected' arcs
+Given a vector of `connected` arcs and a vector of `disconnected` arcs
 recursively add disconnected to connected if they cross with any connected.
 This is done by traversing the crossing graph using depth first search.
 """
@@ -134,10 +159,10 @@ function traverse_crossing_graph_dfs!(connected::PairVector, disconnected::PairV
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Given a vector of pairs, split it into a 'connected' set containing pairs with
-an index <= `k` and a disconnected set containing the rest
+Given a vector of integer pairs, split it into a `connected` set containing pairs with
+an element <= `k` and a disconnected set containing the rest.
 """
 function split_k_connected(pairs::PairVector, k::Int)
     connected = PairVector()
@@ -155,19 +180,19 @@ function split_k_connected(pairs::PairVector, k::Int)
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Given a vector of pairs, count the doubly k-connected ones.
+Given a vector of integer pairs, count the doubly `k`-connected ones.
 """
 function count_doubly_k_connected(pairs::PairVector, k::Int)
     return count(p -> is_doubly_k_connected(p, k), pairs)
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Given a vector of pairs, split it into a 'connected' set containing pairs with
-one index <= `k` and the other index > `k` and a disconnected set containing the rest
+Given a vector of integer pairs, split it into a `connected` set containing pairs with
+one element <= `k` and the other element > `k`, and a disconnected set containing the rest.
 """
 function split_doubly_k_connected(pairs::PairVector, k::Int)
     connected = PairVector()
@@ -185,10 +210,10 @@ function split_doubly_k_connected(pairs::PairVector, k::Int)
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
 Given a topology, check if every connected component of the graph induced by
-crossings between the arcs contains a pair with index <= `k`
+crossings between the arcs contains a pair with an element <= `k`.
 """
 function is_k_connected(t::Topology, k::Int)
     connected, disconnected = split_k_connected(t.pairs, k)
@@ -197,11 +222,11 @@ function is_k_connected(t::Topology, k::Int)
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
 Given a topology, check if every connected component of the graph induced by
-crossings between the arcs contains a pair with one index <= `k` and the other
-index > `k`
+crossings between the arcs contains a pair with one element <= `k` and the other
+element > `k`.
 """
 function is_doubly_k_connected(t::Topology, k::Int)
     connected, disconnected = split_doubly_k_connected(t.pairs, k)
@@ -211,16 +236,16 @@ end
 
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-returns the pair `v[i] => v[j]` and a copy of the vector `v` with elements `i`,`j` removed
+Return the pair `v[i] => v[j]` and a copy of the vector `v` with elements `i`, `j` removed.
 """
 function pop_pair(v::Vector, i, j)
     return v[i] => v[j], [v[k] for k in eachindex(v) if k ∉ (i, j)]
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
 Given a vector of pairs representing a partial partition of the vertices and a
 vector of unpaired vertices, return a vector of complete partitions.
@@ -235,9 +260,9 @@ function pair_partitions(pairs::PairVector, unpaired::Vector{Int})
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Return all partitions of the vertices into pairs.
+Return all possible partitions of an even number of vertices into pairs.
 """
 function pair_partitions(vertices::Vector{Int})
     @assert length(vertices) % 2 == 0
@@ -246,16 +271,16 @@ function pair_partitions(vertices::Vector{Int})
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
-Return all partitions of ``\\{1,...,2n\\}`` into ``n`` pairs.
+Return all partitions of ``\\{1,\\ldots,2n\\}`` into ``n`` pairs.
 """
 function pair_partitions(n::Int)
     return pair_partitions(collect(1:(2n)))
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
 Given a partial topology and a vector of unpaired vertices, return a vector of
 complete topologies, efficiently computing the permutation sign.
@@ -265,7 +290,7 @@ function generate_topologies_impl(topology_partial::Topology, unpaired::Vector{I
     @assert length(unpaired) % 2 == 0
     return map(2:length(unpaired)) do i
         p, rest = pop_pair(unpaired, 1, i)
-        # NOTE each step adds one swap => flips parity
+        # NOTE: each step adds one swap => flips parity
         parity = topology_partial.parity * (-1)^(i)
         return generate_topologies_impl(
             Topology(push!(copy(topology_partial.pairs), p), parity),
@@ -277,7 +302,7 @@ function generate_topologies_impl(topology_partial::Topology, unpaired::Vector{I
 end
 
 """
-$(TYPEDSIGNATURES)
+    $(TYPEDSIGNATURES)
 
 Return topologies of order ``n``, efficiently computing the permutation sign for each.
 """
@@ -286,7 +311,15 @@ function generate_topologies(n::Int)
     return generate_topologies_impl(empty_top, collect(1:(2*n)))
 end
 
-function get_topologies_at_order(order::Int64,
+"""
+    $(TYPEDSIGNATURES)
+
+Return topologies of a given order. If `k` is specified, then only the doubly k-connected
+topologies are returned. With `with_external_arc` set to `true` parity of the returned
+topologies is computed as if an extra arc overarching the last `2order - k` vertices
+was present.
+"""
+function get_topologies_at_order(order::Int,
                                  k = nothing;
                                  with_external_arc = false)::Vector{Topology}
     topologies = generate_topologies(order)
