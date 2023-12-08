265b9084123b97b0bd03819277b241716ff9568d
Hugo U. R. Strand <hugo.strand@gmail.com>
2023-12-08 09:30:00 +0100

    [bench] bethe -> bethe_densmat

--- bench/bethe_densmat/plot_bethe_densmat.py
+++ bench/bethe_densmat/plot_bethe_densmat.py
@@ -13,10 +13,11 @@
 # details.
 #
 # You should have received a copy of the GNU General Public License along with
-# QInchworm.jl. If not, see <http://www.gnu.org/licenses/>.
+# QInchworm.jl. If not, see <http://www.gnu.org/licenses/.
 #
 # Authors: Hugo U. R. Strand, Igor Krivenko
 
+import glob
 import argparse
 
 import numpy as np
@@ -40,11 +41,14 @@ plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
+
 if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument('qinchworm_filename',
-                        type=str,
-                        help="Name of the QInchworm HDF5 results file")
+    parser.add_argument('--qinchworm_filenames',
+                        nargs='+',
+                        default=['data_bethe_ntau_*'],
+                        help="Name of the QInchworm HDF5 results files")
     parser.add_argument('--cthyb_filename',
                         type=str,
                         default="data_bethe_cthyb.h5",
@@ -52,21 +56,25 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    print(f'--> Loading: {args.qinchworm_filename}')
-    with HDFArchive(args.qinchworm_filename, 'r') as a:
-        d = a['data']
-        diffs = d['diffs']
-        N_sampless = d['N_sampless']
-        pto_hist_wrm = d['pto_hists'][-1]
+    args.qinchworm_filenames = sum(list(map(
+        glob.glob, args.qinchworm_filenames)), start=[])
+    
+    diffs, N_sampless = [], []
+    for filename in args.qinchworm_filenames:
+        print(f'--> Loading: {filename}')
+        with HDFArchive(filename, 'r') as a:
+            d = a['data']
+            diffs += list(d['diffs'])
+            N_sampless += list(d['N_sampless'])
 
+    diffs, N_sampless = np.array(diffs), np.array(N_sampless)
+    sidx = np.argsort(N_sampless)
+    diffs, N_sampless = diffs[sidx], N_sampless[sidx]
+    
     print(f'--> Loading: {args.cthyb_filename}')
     with HDFArchive(args.cthyb_filename, 'r') as a:
         ps = a['ps']
-        n_ref = a['n_ref']
-
-    print(f'n_ref 1 = {n_ref:16.16f}')
-    n_ref = 1. - n_ref
-    print(f'n_ref 2 = {n_ref:16.16f}')
+        n_ref = 1 - a['n_ref']
 
     ds = []
     pto_max = 7
@@ -82,7 +90,6 @@ if __name__ == "__main__":
         return np.array([d[name] for d in ds])
 
     n_samples = np.unique(slice_ds("n_samples"))
-    print(n_samples)
 
     err_avgs = np.zeros((len(n_samples)))
     for idx, ns in enumerate(n_samples):
@@ -97,9 +104,6 @@ if __name__ == "__main__":
         x = n_samples
         return np.sum(np.abs(err_avgs / (C / np.sqrt(x)) - 1.))
 
-    res = minimize(target_function, 0.1)
-    print(res)
-
     def target_function_qmc(C):
         x = N_sampless
         y = diffs
@@ -110,78 +114,48 @@ if __name__ == "__main__":
         return np.sum(np.abs(y / (C / x) - 1.))
 
     res = minimize(target_function, 0.1)
-    print(res)
+    res_qmc = minimize(target_function_qmc, 0.001)
 
-    res_qmc = minimize(target_function_qmc, 0.01)
-    print(res_qmc)
+    for label in ['1', '2']:
+        plt.figure(figsize=(3.25, 2.))
 
-    plt.figure(figsize=(3.25, 2.5))
+        gs = GridSpec(
+            1, 1,
+            width_ratios=[1],
+            height_ratios=[1],
+            wspace=0.0, hspace=0.45,
+            bottom=0.14, top=0.99,
+            left=0.16, right=0.99,
+            )
 
-    gs = GridSpec(
-        2, 1,
-        width_ratios=[1],
-        height_ratios=[1, 0.3],
-        wspace=0.0, hspace=0.45,
-        bottom=0.14, top=0.99,
-        left=0.18, right=0.98,
-        )
+        N = np.logspace(0.2, 14, num=10)
+        plt.plot(N, res.x / np.sqrt(N), '--k', lw=0.5)
 
-    plt.subplot(gs[0, 0])
+        if label == '2':
+            plt.plot(N_sampless, diffs, 'o-', markersize=2.5, alpha=0.75, label='Quasi Monte Carlo')
+            plt.text(2e7, 5e-8, r'$\propto 1/N$', ha='center', va='top')
+            N = np.logspace(0.2, 6.5, num=10)
+            plt.plot(N, res_qmc.x / N, ':k', lw=0.5)
+        else:
+            plt.plot([], [], 'o-', markersize=2.5, alpha=0.75, label='Quasi Monte Carlo')
 
-    N = np.logspace(0.2, 6.5, num=10)
-    plt.plot(N, res_qmc.x / N, ':k', lw=0.5)
+        l = plt.plot(slice_ds("n_samples"), errs, 's', alpha=0.02, markersize=2.5)
+        color = l[0].get_color()
+        plt.plot(n_samples, err_avgs, '-', color=color, alpha=0.75)
+        plt.plot([], [], 's-', markersize=2.5, color=color, alpha=0.75, label='Monte Carlo')
 
-    N = np.logspace(0.2, 14, num=10)
-    plt.plot(N, res.x / np.sqrt(N), '--k', lw=0.5,)
+        plt.text(1e8, 2e-4, r'$\propto 1/\sqrt{N}$', ha='left', va='top')
 
-    plt.plot(N_sampless, diffs, '.-', alpha=0.75, label='Quasi Monte Carlo')
+        plt.loglog([],[])
+        plt.grid(True)
+        plt.xlabel(r'Samples $N$')
+        plt.ylabel(r'$|\rho - \rho_{exact}|$')
+        plt.legend(loc='best')
 
-    l = plt.plot(slice_ds("n_samples"), errs, '.', alpha=0.02)
-    color = l[0].get_color()
-    plt.plot(n_samples, err_avgs, '-', color=color, alpha=0.75)
-    plt.plot([], [], '.-', color=color, alpha=0.75, label='Monte Carlo')
+        plt.axis('equal')
+        plt.ylim([1e-8, 1e0])
 
-    plt.text(1e8, 2e-4, r'$\propto 1/\sqrt{N}$', ha='left', va='top')
-    plt.text(2e7, 5e-8, r'$\propto 1/N$', ha='center', va='top')
+        plt.tight_layout()
+        plt.savefig(f'figure_mc_convergence_{label}.pdf')
 
-    plt.loglog([],[])
-    plt.xlabel(r'Samples $N$')
-    plt.ylabel(r'Error in $\rho$')
-    plt.legend(loc='best')
-
-    plt.xlim(left=1, right=1e15)
-    plt.ylim(bottom=1e-8)
-
-    plt.subplot(gs[1, 0])
-
-    p = ps[-1]
-
-    n = 0
-    pto = np.zeros_like(ds[-1]["pto"])
-    for d in ds:
-        if d["n_samples"] == n_samples[-1]:
-            pto += d["pto"]
-            n += 1
-    pto /= n
-
-    x = np.arange(0, len(pto))
-    pto /= np.sum(pto)
-
-    color_wrm = plt.plot([], [])[0].get_color()
-    color_cth = plt.plot([], [])[0].get_color()
-
-    plt.bar(x, pto, alpha=0.75, color=color_cth)
-    pto_hist_wrm = np.abs(pto_hist_wrm)
-    pto_hist_wrm /= np.sum(pto_hist_wrm)
-    plt.bar(range(len(pto_hist_wrm)), pto_hist_wrm, alpha=0.75, color=color_wrm)
-
-    plt.semilogy([], [])
-    plt.xlabel('Perturbation order')
-    plt.ylabel('Probabilbity')
-    plt.xticks(np.arange(0, pto_max), labels=[f'{i:d}' for i in range(0, pto_max)])
-    plt.ylim(bottom=5e-8, top=10)
-    plt.yticks([1e-6, 1e-3, 1e0])
-
-    plt.tight_layout()
-    plt.savefig('figure_mc_convergence.pdf')
     plt.show()
