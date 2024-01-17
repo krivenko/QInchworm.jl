1935b09afb9e0c0f9117e41462ba6b2a06fabe50
Igor Krivenko <igor.s.krivenko@gmail.com>
2024-01-17 15:50:24 +0100

    Added shell script that updates copyright years in GPL headers

---
+++ tools/update-copyright-years.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+#
+# Update copyright years in files
+#
+
+FIND=$(which find)
+SED=$(which sed)
+
+# Collect paths of files to be updated
+FILES="README.md"
+for PATTERN in "*.jl" "*.py"
+do
+    FILES="$FILES $($FIND . -name $PATTERN)"
+done
+
+# Write the new year string
+YEARS_REGEX="([0-9]{4})-[0-9]{4}"
+YEARS_UPDATED="\\1-$(date +%Y)"
+
+COPYRIGHT_REGEX="Copyright \(C\) ${YEARS_REGEX}"
+COPYRIGHT_UPDATED="Copyright \(C\) ${YEARS_UPDATED}"
+for FILE in $FILES
+do
+    $SED -i -E "s/${COPYRIGHT_REGEX}/${COPYRIGHT_UPDATED}/g" $FILE
+done
