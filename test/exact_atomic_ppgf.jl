6dd8273e70bb6df299f3d08ed5b26c5cbcae04d4
Igor Krivenko <igor.s.krivenko@gmail.com>
2024-07-09 15:54:31 +0200

    [test] Minor cleanup of `exact_atomic_ppgf`

--- test/exact_atomic_ppgf.jl
+++ test/exact_atomic_ppgf.jl
@@ -20,8 +20,6 @@
 # Test exact atomic pseudo particle Green's function (exact_atomic_ppgf) module
 # by comparing with the standard equidistant constructor in the ppgf module.
 
-#using PyPlot; plt = PyPlot
-
 using Test
 
 using Keldysh; kd = Keldysh
@@ -31,11 +29,11 @@ using QInchworm.ppgf: atomic_ppgf, partition_function
 using QInchworm.exact_atomic_ppgf: exact_atomic_ppgf, partition_function
 
 @testset "exact_atomic_ppgf" begin
-    
+
     β = 5.0
     nτ = 11
     μ = 1.337
-    
+
     H = μ * op.n(1)
     soi = ked.Hilbert.SetOfIndices([[1]])
     ed = ked.EDCore(H, soi)
@@ -43,43 +41,20 @@ using QInchworm.exact_atomic_ppgf: exact_atomic_ppgf, partition_function
     P = atomic_ppgf(β, ed)
     Z = partition_function(P)
     @test Z ≈ 1.0
-    
+
     contour = kd.ImaginaryContour(β=β)
     grid = kd.ImaginaryTimeGrid(contour, nτ)
     P0 = atomic_ppgf(grid, ed)
     Z0 = partition_function(P0)
     @test Z == Z0
-    
+
     t0 = grid.points[1].bpoint
-    
+
     for t1 in grid.points
         t1 = t1.bpoint
-        for s in length(P)
-            @test P[s](t1, t0) == P0[s](t1, t0)
+        for (p, p0) in zip(P, P0)
+            @test p(t1, t0) == p0(t1, t0)
         end
     end
 
-    if false
-        τ = [ -imag(t.bpoint.val) for t in grid.points ]
-
-        p1 = Array{ComplexF64, 3}(undef, 1, 1, nτ)
-        p2 = Array{ComplexF64, 3}(undef, 1, 1, nτ)
-        
-        t0 = grid.points[1].bpoint
-        for (idx, t1) in enumerate(grid.points)
-            t1 = t1.bpoint
-            p1[:, :, idx] = P[1](t1, t0)
-            p2[:, :, idx] = P[2](t1, t0)
-        end
-
-        plt.plot(τ, -imag(P0[1].mat.data[1, 1, :]), "x-")
-        plt.plot(τ, -imag(P0[2].mat.data[1, 1, :]), "x-")
-        
-        plt.plot(τ, -imag(p1[1, 1, :]), "+-")
-        plt.plot(τ, -imag(p2[1, 1, :]), "+-")
-        
-        plt.ylim([-0.1, 1.1])
-        plt.show()
-    end
-
-end # testset
+end
